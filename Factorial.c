738cbcd19df63543d7e6214a2b79e7acaa088cfe
VishishtaShukla <vishishtashukla1@gmail.com>
2026-02-11 00:03:22 +0530

    Add files via upload

---
+++ Factorial.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n,factorial=1;
+    printf("Enter number");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+       factorial=factorial*i;
+    }
+    printf("Factorial= %d",factorial);
+    return 0;
+}
